d757444880c2c2baea3e1301cf8ca894ae6d8df2
miao-wang-au <40533399+miao-wang-au@users.noreply.github.com>
2024-07-17 00:44:45 +0200

    Add files via upload

--- SISK2.py
+++ SISK2.py
@@ -1,5 +1,6 @@
 import time
-from datetime import date
+from UserLogin import Login
+from datetime import datetime
 
 from selenium.webdriver import Keys
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,34 +14,7 @@ driver.set_window_position(2000, 0)
 driver.maximize_window()
 driver.get("https://aims-sisk-ui-test.aga.rbxd.ds/login")
 
-driver.find_element(By.NAME, "username").send_keys("f4fadmin@f4f.com")
-driver.find_element(By.NAME, "password").send_keys("marDzBHU35wB")
-
-# XPATH -> //tagname[@attribute='value'] -> //input[@type='submit']
-# CSS -> tagname[attribute='value'] -> //input[type='submit']
-
-driver.find_element(By.XPATH, "//button[@type='submit']").click()
-
-WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, "//button[@data-testid='selectSupplier13']"))
-)
-driver.find_element(By.XPATH, "//button[@data-testid='selectSupplier13']").click()
-
-WebDriverWait(driver, 5)
-
-driver.find_element(By.XPATH, "//*[contains(text(), 'Submit')]").click() # The only way that has worked so far
-
-WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.XPATH, "//button[@title='Open']"))
-)
-
-driver.find_element(By.XPATH, "//button[@title='Open']").click()
-
-# Click dropdown 'mater Private Hospital'
-WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Mater Private Hospital')]"))
-)
-driver.find_element(By.XPATH, "//*[contains(text(), 'Mater Private Hospital')]").click()
+Login("Hospital Theater", driver)
 
 WebDriverWait(driver, 15).until(
     EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Stock Levels')]"))
@@ -111,15 +85,24 @@ requisitioncreated = driver.find_element(By.XPATH, "(//div[@role='alert'])[1]").
 
 print(requisitioncreated)
 
+driver.find_element(By.XPATH, "(//button[@title='Account settings'])[1]").click()
+driver.find_element(By.XPATH, "(//li[normalize-space()='Logout'])[1]").click()
+
 ###############################################################################
+
+Login("Customer Service", driver)
+
+WebDriverWait(driver, 10).until(
+     EC.presence_of_element_located((By.LINK_TEXT, "Requisitions"))
+)
 
 driver.find_element(By.LINK_TEXT, "Requisitions").click()
 
 WebDriverWait(driver, 10).until(
-     EC.presence_of_element_located((By.CSS_SELECTOR, "tbody tr:nth-child(1) td:nth-child(6) button:nth-child(2)"))
+     EC.presence_of_element_located((By.XPATH, "(//button[@type='button'])[10]"))
 )
 
-driver.find_element(By.CSS_SELECTOR, "tbody tr:nth-child(1) td:nth-child(6) button:nth-child(2)").click()
+driver.find_element(By.XPATH, "(//button[@type='button'])[10]").click()
 
 WebDriverWait(driver, 10).until(
      EC.presence_of_element_located((By.XPATH, "(//button[@type='button'])[44]"))
@@ -139,7 +122,7 @@ WebDriverWait(driver, 10).until(
      EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Stock Levels')]"))
 )
 
-time.sleep(5)
+WebDriverWait(driver, 5)
 
 driver.find_element(By.XPATH, "//*[contains(text(), 'Stock Levels')]").click()
 
